5f3eef62732dcbc45fbc92df6774b58af4d113c4
Farheen Imam <115992935+farheenimam@users.noreply.github.com>
2025-09-09 16:03:04 +0500

    Time: 38 ms (43.2%), Space: 13.3 MB (51.17%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        m = 0
+           
+        for i in range(len(nums)):
+            if (m < i):
+                return False 
+            m = max(m, nums[i] + i)  
+            
+        return True    
