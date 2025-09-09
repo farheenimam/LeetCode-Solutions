563193d01400acf835a0c2655a96e5dec2b76ece
Farheen Imam <115992935+farheenimam@users.noreply.github.com>
2025-09-09 16:01:39 +0500

    Time: 7 ms (10.73%), Space: 18.3 MB (5.19%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        count = 0
+        citations = sorted(citations, reverse=True)
+        for i in range(0, len(citations)):
+            if citations[i] >= i+1:
+                count +=1
+
+        return count
